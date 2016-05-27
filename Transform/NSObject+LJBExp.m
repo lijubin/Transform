60362c9d7814de926c830cb2c01f975f6f0f28d5
lijubin <lijubin@users.noreply.github.com>
2016-05-27 10:57:43 +0800

    Update NSObject+LJBExp.m
    增加数据缓存！

--- Transform/NSObject+LJBExp.m
+++ Transform/NSObject+LJBExp.m
@@ -54,7 +54,7 @@
                 [object setValue:dict[dictKey] forKey:mapKey];
             }
         }  else if ([dict[dictKey] isEqual:[NSNull null]]
-                   || dict[dictKey] == nil) {
+                    || dict[dictKey] == nil) {
             ivarType = [ivarType stringByReplacingOccurrencesOfString:@"@\"" withString:@""];
             ivarType = [ivarType stringByReplacingOccurrencesOfString:@"\"" withString:@""];
             // 获取类
@@ -88,5 +88,77 @@
     return resultArray;
 }
 
+#pragma mark - 数据缓存
++ (void)writeDataCache:(NSString *)dataCacheId dataLists:(NSArray *)dataLists {
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
+    if (dataLists == nil
+        || dataLists.count == 0) {
+        [userDefault removeObjectForKey:dataCacheId];
+    } else {
+        [userDefault removeObjectForKey:dataCacheId];
+        NSMutableArray *dataArray = [NSMutableArray array];
+        for (int i = 0; i < (dataLists.count > 10 ? 10 : dataLists.count); i++) {
+            NSDictionary *dataDict = dataLists[i];
+            NSMutableDictionary *resultDict = [NSMutableDictionary dictionary];
+            //处理 需要存储的字典信息
+            if ([dataDict isEqual:[NSNull null]]
+                || !dataDict) {
+                continue;
+            }
+            unsigned int count = 0;
+            Ivar *ivarList = class_copyIvarList(self, &count);
+            for (unsigned int i = 0; i < count; i++) {
+                Ivar ivar = ivarList[i];
+                NSString *mapKey = [NSString stringWithUTF8String:ivar_getName(ivar)];
+                NSString *dictKey = mapKey;
+                if ([mapKey hasPrefix:@"_"]) {
+                    NSMutableString *tmpStr = [NSMutableString stringWithString:mapKey];
+                    int lenght = 0;
+                    for (int i = 0; i < tmpStr.length; i++) {
+                        if ([[tmpStr substringWithRange:NSMakeRange(i, 1)] isEqualToString:@"_"]) {
+                            lenght++;
+                        } else {
+                            break;
+                        }
+                    }
+                    NSRange range = NSMakeRange(0, lenght);
+                    [tmpStr replaceCharactersInRange:range withString:@""];
+                    dictKey = tmpStr;
+                }
+                //模型转字典
+                
+                if (![dataDict[dictKey] isEqual:[NSNull null]]
+                    && dataDict[dictKey]) {
+                    if ([dataDict[dictKey] isKindOfClass:[NSString class]]
+                        && [dataDict[dictKey] isEqual:@""]) {
+                        continue;
+                    }
+                    if ([dataDict[dictKey] isKindOfClass:[NSNumber class]]
+                        && ([dataDict[dictKey] isEqual:@""])) {
+                        continue;
+                    }
+                    resultDict[dictKey] = dataDict[dictKey];
+                }
+            }
+            if (resultDict) {
+                [dataArray addObject:resultDict];
+            }
+        }
+        [userDefault setObject:dataArray forKey:dataCacheId];
+    }
+}
+
+#pragma mark - 读取缓存数据
++ (NSArray *)readDataCache:(NSString *)dataCacheId {
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
+    NSArray *tmpArray = [userDefault objectForKey:dataCacheId];
+    NSArray *resultArray = nil;
+    if (tmpArray == nil || tmpArray.count == 0) {
+        resultArray = nil;
+    } else {
+        resultArray = [self ljbObjectWithArray:tmpArray];
+    }
+    return resultArray;
+}
 
 @end
