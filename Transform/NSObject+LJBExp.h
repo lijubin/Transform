6b89522b4fb40715908089d971392d796f2980e6
lijubin <lijubin@users.noreply.github.com>
2016-05-27 10:58:11 +0800

    Update NSObject+LJBExp.h

--- Transform/NSObject+LJBExp.h
+++ Transform/NSObject+LJBExp.h
@@ -1,19 +1,41 @@
 //
 //  NSObject+LJBExp.h
-//  Transform
+//  一公里
 //
-//  Created by 李居彬 on 16/1/25.
-//  Copyright © 2016年 ljb. All rights reserved.
+//  Created by liuguopan on 16/1/5.
+//  Copyright © 2016年 viewcreator3d. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSObject (LJBExp)
 
-/**字典转模型*/
+/**
+ *  字典转模型
+ *
+ *  @param dict 需要转换的字典
+ *
+ *  @return 返回模型数据
+ */
 + (instancetype)ljbObjectWithDict:(NSDictionary *)dict;
 
-/**数组转模型*/
+/**
+ *  数组转模型
+ *
+ *  @param array 数组中含有需要转换的字典
+ *
+ *  @return 含有转换后的模型的数组
+ */
 + (NSArray *)ljbObjectWithArray:(NSArray *)array;
+
+/**
+ *  数据列表缓存
+ *
+ *  @param dataCacheId 数据Id
+ *  @param dataLists   数据信息
+ */
++ (void)writeDataCache:(NSString *)dataCacheId dataLists:(NSArray *)dataLists;
+
++ (NSArray *)readDataCache:(NSString *)dataCacheId;
 
 @end
